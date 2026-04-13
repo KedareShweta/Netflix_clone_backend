c9d4caf92564b2a2a2ef3d7ee804ff7fa776cc4f
Shweta Kedare <shwetakedare96gitproject@gmail.com>
2026-04-13 11:20:58 +0530

    user

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,65 @@
 import { User } from "../models/userModel.js";
-import bcryptjs from "bcrypt"
+import bcryptjs from "bcrypt";
+import jwt from "jsonwebtoken";
+
+export const Login = async(req,res)=>{
+    try{
+        // take mail and password
+        const {email,password} = req.body;
+        // if no maild id or password are not matched with eachpther
+        if(!email || !password){
+            return res.status(401).json({
+                message:"Invalid data",
+                success:false
+            })
+        };
+        // find user through email id in database 
+        const user = await User.findOne({email});
+        // if no user found i.e email found (email is imp cos user cannot use 1 mail id more than one time)
+        if(!user){
+            return res.status(401).json({
+            message:"Invalid email or password",
+            success: false    
+            });
+        }
+        // see whether fronend password is matching with database hashed password or not
+        const isMatch = await bcryptjs.compare(password, user.password);
+        
+        // frontend password != database password
+        if(!isMatch){
+            return res.status(401).json({
+                message:"Invalid email or password",
+                success: false
+            });
+        }
+
+        // frontend password == database password
+        // then will genrate token i.e jwt
+        // cos on the basis of token we are going to decide whether user is logged in ot not
+
+        const tokenData = {
+            id:user._id
+        }
+        const token = await jwt.sign(tokenData, "blhblfvblfre",{expiresIn:"1d"});
+
+        return res.status(200).cookie("token", token,{httpOnly:true}).json({
+            message:`Welcome back ${user.fullName}`,
+            success:true
+        });
+
+    }catch(error){
+        console.log(error);
+        
+    }
+}
+
+export const Logout = async (req,res) => {
+    return res.status(200).cookie("token", "", {expiresIn:new Date(Date.now()), httpOnly:true}).json({
+        message:"User logged out successfully.",
+        success:true,
+    });
+}
+
 export const Register = async(req,res) => {
     try{
 
